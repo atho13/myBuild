54af41b355fb2cf5e08070688a2ab8f800b3aac8
xXx <181945610+ribel13@users.noreply.github.com>
2026-03-20 14:00:37 +0900

    Update x86.sh

--- config/imagebuilder/x86.sh
+++ config/imagebuilder/x86.sh
@@ -31,39 +31,19 @@ if [ "$TARGET_TYPE" == "generic" ]; then
 fi
 
 # 3. KONFIGURASI OTOMATIS (IP STATIS & BBR)
-mkdir -p files/etc/uci-defaults
-cat <<EOF > files/etc/uci-defaults/99-custom-settings
 #!/bin/sh
 # 1. Aktifkan Google BBR
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
 
-# 2. Pengaturan Network (IP Statis 192.168.1.1)
-# Menghapus jembatan (bridge) jika ada dan mengatur eth0 sebagai LAN utama
-uci set network.lan.device='eth0'
-uci set network.lan.proto='static'
-uci set network.lan.ipaddr='192.168.1.1'
-uci set network.lan.netmask='255.255.255.0'
-uci set network.lan.gateway='192.168.1.1'
-uci set network.lan.dns='8.8.8.8 1.1.1.1'
-
-# Simpan dan terapkan
-uci commit network
-/etc/init.d/network restart
-
-# 3. Jalankan fstrim jika x86
-[ -x /sbin/fstrim ] && /sbin/fstrim -av
-exit 0
-EOF
-chmod +x files/etc/uci-defaults/99-custom-settings
-
 # 4. EKSEKUSI BUILD (PROFILE generic)
 log "INFO: Memulai proses Build Firmware..."
-
+[ -d "${GITHUB_WORKSP 5gACE}/files" ] && chmod -R +x "${GITHUB_WORKSPACE}/files/etc/uci-defaults"
 make image PROFILE="generic" \
            PACKAGES="$PACKAGES" \
            FILES="files" \
+           V=s \
            CONFIG_TARGET_ROOTFS_TARGZ=y \
            CONFIG_TARGET_KERNEL_PARTSIZE=256 \
            CONFIG_TARGET_ROOTFS_PARTSIZE=750
