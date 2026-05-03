aa966b924a08e528d74ebb7ca5b2436dad864703
Atbel <kanaan.waropen@gmail.com>
2026-05-03 12:52:07 +0900

    Update frdmx.sh

--- config/imagebuilder/frdmx.sh
+++ config/imagebuilder/frdmx.sh
@@ -19,7 +19,8 @@ sudo chown -R runner:runner "${make_path}"
 # 2. Download ImageBuilder (URL ARMSr/ARMv8)
 download_imagebuilder() {
     cd "${make_path}"
-    echo -e "${STEPS} Mengunduh ImageBuilder OpenWrt 25.12.2 (ARMSR/ARMV8)..."
+    #echo -e "${STEPS} Mengunduh ImageBuilder OpenWrt 25.12.2 (ARMSR/ARMV8)..."
+    echo -e "${STEPS} Mengunduh ImageBuilder ImmortalWrt 24.10.6 (ARMSR/ARMV8)..."
     
     #URL="https://downloads.openwrt.org/releases/25.12.2/targets/armsr/armv8/openwrt-imagebuilder-25.12.2-armsr-armv8.Linux-x86_64.tar.zst"
     URL="https://downloads.immortalwrt.org/releases/24.10.6/targets/armsr/armv8/immortalwrt-imagebuilder-24.10.6-armsr-armv8.Linux-x86_64.tar.zstx_tr_hl=id&_x_tr_pto=tc"
