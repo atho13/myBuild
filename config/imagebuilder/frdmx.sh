bb04480fcaa7421f6bdd4481b31d966266fbcc5f
Atbel <kanaan.waropen@gmail.com>
2026-07-30 12:30:21 +0900

    Update frdmx.sh

--- config/imagebuilder/frdmx.sh
+++ config/imagebuilder/frdmx.sh
@@ -44,23 +44,23 @@ rebuild_firmware() {
     echo -e "${STEPS} Membangun Rootfs ARMSR..."
 
     my_packages="-dnsmasq dnsmasq-full base-files dropbear e2fsprogs firewall4 fstools tc-full \
-          kmod-button-hotplug kmod-nft-offload libc libgcc libustream-mbedtls logd kmod-tcp-bbr \
-          mkf2fs mtd netifd nftables odhcp6c odhcpd-ipv6only partx-utils ppp ppp-mod-pppoe procd-ujail \
-          uci uclient-fetch urandom-seed urngd luci luci-compat luci-lib-base kmod-usb-net-huawei-cdc-ncm \
-          kmod-usb-net kmod-usb-net-rndis luci-lib-ip luci-lib-jsonc luci-lib-nixio luci-mod-admin-full \
+          kmod-button-hotplug kmod-nft-offload libc libgcc libustream-mbedtls logd mkf2fs mtd netifd \
+          nftables odhcp6c odhcpd-ipv6only partx-utils ppp ppp-mod-pppoe procd-ujail uci uclient-fetch \
+          urandom-seed urngd luci luci-compat luci-lib-base kmod-usb-net-huawei-cdc-ncm kmod-usb-net \
+          kmod-usb-net-rndis luci-lib-ip luci-lib-jsonc luci-lib-nixio luci-mod-admin-full unzip mbim-utils sms-tool \
           luci-mod-network kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 kmod-usb-net-asix kmod-usb-net-asix-ax88179 \
-          kmod-mii luci-mod-status luci-mod-system luci-proto-3g luci-proto-mbim mbim-utils picocom minicom \
+          kmod-mii luci-mod-status luci-mod-system luci-proto-3g luci-proto-mbim picocom minicom qmi-utils \
           luci-proto-ncm luci-proto-ppp luci-proto-qmi screen kmod-tun ttyd kmod-usb-atm kmod-macvlan \
           kmod-usb-net-cdc-ncm kmod-usb-net-cdc-mbim luci-proto-modemmanager modemmanager modemmanager-rpcd \
-          libqmi libmbim glib2 ipset libcap libcap-bin ruby ruby-yaml kmod-inet-diag kmod-nft-tproxy \
-          ip-full php8 tcpdump irqbalance bc uhttpd uhttpd-mod-ubus unzip qmi-utils kmod-usb-net-qmi-wwan \
-          uqmi usb-modeswitch uuidgen zstd wwan ziptool zoneinfo-asia zoneinfo-core zram-swap bash ca-bundle \
-          openssh-sftp-server adb wget-ssl httping htop jq tar coreutils-sleep coreutils-stat nano fping \
-          kmod-nls-utf8 kmod-usb-storage cgi-io chattr comgt comgt-ncm coremark coreutils coreutils-base64 \
-          coreutils-nohup kmod-usb-net-sierrawireless kmod-usb-serial-qualcomm kmod-usb-serial-sierrawireless \
-          luci-app-ttyd luci-theme-material iw iwinfo netdata vnstat2 vnstati2 php8-mod-mbstring php8-cli \
-          php8-fastcgi php8-fpm php8-mod-session php8-mod-ctype php8-mod-fileinfo php8-mod-zip php8-mod-iconv \
-          kmod-mhi-net kmod-mhi-bus kmod-mhi-pci-generic kmod-mhi-wwan-ctrl kmod-mhi-wwan-mbim kmod-sched-cake"
+          libqmi libmbim glib2 ipset libcap libcap-bin kmod-inet-diag kmod-nft-tproxy wwan uhttpd \
+          ip-full php8 bc uhttpd-mod-ubus kmod-usb-net-qmi-wwan uqmi usb-modeswitch uuidgen \
+          zstd ziptool zoneinfo-asia zoneinfo-core bash openssh-sftp-server adb wget-ssl httping htop \
+          jq tar coreutils-sleep coreutils-stat nano fping kmod-nls-utf8 kmod-usb-storage cgi-io chattr comgt \
+          comgt-ncm coremark coreutils coreutils-base64 coreutils-nohup kmod-usb-net-sierrawireless \
+          kmod-usb-serial-qualcomm kmod-usb-serial-sierrawireless luci-app-ttyd luci-theme-material \
+          netdata vnstat2 vnstati2 php8-mod-mbstring php8-cli php8-fastcgi php8-fpm php8-mod-session \
+          php8-mod-ctype php8-mod-fileinfo php8-mod-zip php8-mod-iconv kmod-sched-cake \
+          kmod-mhi-net kmod-mhi-bus kmod-mhi-pci-generic kmod-mhi-wwan-ctrl kmod-mhi-wwan-mbim"
         
     [ -d "${GITHUB_WORKSPACE}/files" ] && chmod -R +x "${GITHUB_WORKSPACE}/files/etc/uci-defaults"
     make image PROFILE="generic" \
