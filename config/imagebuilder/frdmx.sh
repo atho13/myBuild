9a121024ea5d4e460cc5a472af0277501fc56db6
Atbel <kanaan.waropen@gmail.com>
2026-08-03 08:55:09 +0900

    Update frdmx.sh

--- config/imagebuilder/frdmx.sh
+++ config/imagebuilder/frdmx.sh
@@ -15,10 +15,10 @@ sudo chown -R runner:runner "${make_path}"
 
 download_imagebuilder() {
     cd "${make_path}"
-    echo -e "${STEPS} Mengunduh ImageBuilder OpenWrt 24.10.8 (ARMSR/ARMV8)..."
+    echo -e "${STEPS} Mengunduh ImageBuilder OpenWrt 24.10.3 (ARMSR/ARMV8)..."
     # echo -e "${STEPS} Mengunduh ImageBuilder ImmortalWrt 24.10.6 (ARMSR/ARMV8)..."
     
-    URL="https://downloads.openwrt.org/releases/24.10.8/targets/armsr/armv8/openwrt-imagebuilder-24.10.8-armsr-armv8.Linux-x86_64.tar.zst"
+    URL="https://downloads.openwrt.org/releases/24.10.3/targets/armsr/armv8/openwrt-imagebuilder-24.10.3-armsr-armv8.Linux-x86_64.tar.zst"
     # URL="https://downloads.immortalwrt.org/releases/24.10.6/targets/armsr/armv8/immortalwrt-imagebuilder-24.10.8-armsr-armv8.Linux-x86_64.tar.zst"
     
     curl -fL -o ib.tar.zst "$URL" || { echo -e "${ERROR} Gagal download!"; exit 1; }
@@ -44,28 +44,29 @@ rebuild_firmware() {
     echo -e "${STEPS} Membangun Rootfs ARMSR..."
 
     my_packages="-dnsmasq dnsmasq-full base-files dropbear e2fsprogs firewall4 fstools tc-full \
-          kmod-button-hotplug kmod-nft-offload libc libgcc libustream-mbedtls logd mkf2fs mtd netifd \
-          nftables odhcp6c odhcpd-ipv6only partx-utils ppp ppp-mod-pppoe procd-ujail uci uclient-fetch \
-          urandom-seed urngd luci luci-compat luci-lib-base kmod-usb-net-huawei-cdc-ncm kmod-usb-net \
-          kmod-usb-net-rndis luci-lib-ip luci-lib-jsonc luci-lib-nixio luci-mod-admin-full unzip mbim-utils sms-tool \
+          kmod-button-hotplug kmod-nft-offload libc libgcc libustream-mbedtls logd kmod-tcp-bbr \
+          mkf2fs mtd netifd nftables odhcp6c odhcpd-ipv6only partx-utils ppp ppp-mod-pppoe procd-ujail \
+          uci uclient-fetch urandom-seed urngd luci luci-compat luci-lib-base kmod-usb-net-huawei-cdc-ncm \
+          kmod-usb-net kmod-usb-net-rndis luci-lib-ip luci-lib-jsonc luci-lib-nixio luci-mod-admin-full \
           luci-mod-network kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 kmod-usb-net-asix kmod-usb-net-asix-ax88179 \
-          kmod-mii luci-mod-status luci-mod-system luci-proto-3g luci-proto-mbim picocom minicom qmi-utils \
+          kmod-mii luci-mod-status luci-mod-system luci-proto-3g luci-proto-mbim mbim-utils picocom minicom \
           luci-proto-ncm luci-proto-ppp luci-proto-qmi screen kmod-tun ttyd kmod-usb-atm kmod-macvlan \
-          kmod-usb-net-cdc-ncm kmod-usb-net-cdc-mbim luci-proto-modemmanager modemmanager modemmanager-rpcd \
-          libqmi libmbim glib2 ipset libcap libcap-bin kmod-inet-diag kmod-nft-tproxy wwan uhttpd \
-          ip-full php8 bc uhttpd-mod-ubus kmod-usb-net-qmi-wwan uqmi usb-modeswitch uuidgen \
-          zstd ziptool zoneinfo-asia zoneinfo-core bash openssh-sftp-server adb wget-ssl httping htop \
-          jq tar coreutils-sleep coreutils-stat nano fping kmod-nls-utf8 kmod-usb-storage cgi-io chattr comgt \
-          comgt-ncm coremark coreutils coreutils-base64 coreutils-nohup kmod-usb-net-sierrawireless \
-          kmod-usb-serial-qualcomm kmod-usb-serial-sierrawireless luci-app-ttyd luci-theme-material \
-          netdata vnstat2 vnstati2 php8-mod-mbstring php8-cli php8-fastcgi php8-fpm php8-mod-session \
-          php8-mod-ctype php8-mod-fileinfo php8-mod-zip php8-mod-iconv kmod-sched-cake \
-          kmod-mhi-net kmod-mhi-bus kmod-mhi-pci-generic kmod-mhi-wwan-ctrl kmod-mhi-wwan-mbim"
+          kmod-usb-net-cdc-ncm kmod-usb-net-cdc-mbim luci-proto-modemmanager modemmanager modemmanager-rpcd qmi-utils \
+          libqmi libmbim glib2 ipset libcap libcap-bin ruby ruby-yaml kmod-inet-diag kmod-nft-tproxy mbim-utils sms-tool \
+          ip-full php8 tcpdump irqbalance bc uhttpd uhttpd-mod-ubus unzip qmi-utils kmod-usb-net-qmi-wwan \
+          uqmi usb-modeswitch uuidgen zstd wwan ziptool zoneinfo-asia zoneinfo-core zram-swap bash \
+          openssh-sftp-server adb wget-ssl httping htop jq tar coreutils-sleep coreutils-stat nano fping \
+          kmod-nls-utf8 kmod-usb-storage cgi-io chattr comgt comgt-ncm coremark coreutils coreutils-base64 \
+          coreutils-nohup kmod-usb-net-sierrawireless kmod-usb-serial-qualcomm kmod-usb-serial-sierrawireless \
+          luci-app-ttyd luci-theme-material iw iwinfo netdata vnstat2 vnstati2 php8-mod-mbstring php8-cli \
+          php8-fastcgi php8-fpm php8-mod-session php8-mod-ctype php8-mod-fileinfo php8-mod-zip php8-mod-iconv \
+          kmod-mhi-net kmod-mhi-bus kmod-mhi-pci-generic kmod-mhi-wwan-ctrl kmod-mhi-wwan-mbim kmod-sched-cake"
         
     [ -d "${GITHUB_WORKSPACE}/files" ] && chmod -R +x "${GITHUB_WORKSPACE}/files/etc/uci-defaults"
     make image PROFILE="generic" \
                PACKAGES="${my_packages}" \
-               FILES="${GITHUB_WORKSPACE}/files" \
+               FILES="${{ github.workspace }}/files" \
+               IGNORE_ERRORS=1 \
                V=s
                
     if [ $? -eq 0 ]; then
