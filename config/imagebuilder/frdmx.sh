3c8fcb2a1eb5e8670758d12270f4aed06ae4de4c
Atbel <kanaan.waropen@gmail.com>
2026-08-05 12:27:46 +0900

    Update frdmx.sh

--- config/imagebuilder/frdmx.sh
+++ config/imagebuilder/frdmx.sh
@@ -15,10 +15,10 @@ sudo chown -R runner:runner "${make_path}"
 
 download_imagebuilder() {
     cd "${make_path}"
-    echo -e "${STEPS} Mengunduh ImageBuilder OpenWrt 24.10.5 (ARMSR/ARMV8)..."
+    echo -e "${STEPS} Mengunduh ImageBuilder OpenWrt 24.10.8 (ARMSR/ARMV8)..."
     # echo -e "${STEPS} Mengunduh ImageBuilder ImmortalWrt 24.10.6 (ARMSR/ARMV8)..."
     
-    URL="https://downloads.openwrt.org/releases/24.10.5/targets/armsr/armv8/openwrt-imagebuilder-24.10.5-armsr-armv8.Linux-x86_64.tar.zst"
+    URL="https://downloads.openwrt.org/releases/24.10.8/targets/armsr/armv8/openwrt-imagebuilder-24.10.8-armsr-armv8.Linux-x86_64.tar.zst"
     # URL="https://downloads.immortalwrt.org/releases/24.10.6/targets/armsr/armv8/immortalwrt-imagebuilder-24.10.8-armsr-armv8.Linux-x86_64.tar.zst"
     
     curl -fL -o ib.tar.zst "$URL" || { echo -e "${ERROR} Gagal download!"; exit 1; }
