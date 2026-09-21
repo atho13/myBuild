840ec467c5189815d5b0ad95f68d053a78a1eec4
Atbel <kanaan.waropen@gmail.com>
2026-09-21 20:21:08 +0900

    Update free.sh

--- files/sbin/free.sh
+++ files/sbin/free.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
-# frdmx cleaner + Automatic ZRAM Init - Optimized for Performance
+# For Frdmx-Wrt Community
 
 if ! swapon -s | grep -q "zram"; then
     zramctl /dev/zram0 --size 256M --algorithm lz4 2>/dev/null
     mkswap /dev/zram0 2>/dev/null
     swapon -p 32767 /dev/zram0 2>/dev/null
     
+    sysctl -w vm.swappiness=60 >/dev/null 2>&1
+    logger -t "Frdmx-Cleaner" "[+] Sukses mengaktifkan ZRAM 256MB (LZ4) & Optimasi Swappiness."
 else
-
-    swapoff -a && swapon -a
+    logger -t "Frdmx-Cleaner" "[*] Info: ZRAM sudah aktif dan berjalan normal."
 fi
 
 sync
-echo 3 > /proc/sys/vm/drop_caches
+echo 3 > /proc/sys/vm/drop_caches 2>/dev/null
+logger -t "Frdmx-Cleaner" "[+] Memori cache sistem berhasil dibersihkan."
 
 for LOG_DIR in /tmp/log /var/log; do
     if [ -d "$LOG_DIR" ]; then
