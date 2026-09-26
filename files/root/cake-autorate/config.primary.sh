44a4f606471c30774873a4195e7d87686a3cb1e2
Atbel <kanaan.waropen@gmail.com>
2026-09-26 13:05:53 +0900

    Update config.primary.sh

--- files/root/cake-autorate/config.primary.sh
+++ files/root/cake-autorate/config.primary.sh
@@ -8,16 +8,17 @@ dl_if="lo"
 adjust_dl_shaper_rate=1 
 adjust_ul_shaper_rate=1 
 
-min_dl_shaper_rate_kbps=10000
-base_dl_shaper_rate_kbps=200000
-max_dl_shaper_rate_kbps=400000
+min_dl_shaper_rate_kbps=3000
+base_dl_shaper_rate_kbps=15000
+max_dl_shaper_rate_kbps=80000
 
-min_ul_shaper_rate_kbps=5000
-base_ul_shaper_rate_kbps=80000
-max_ul_shaper_rate_kbps=150000
+min_ul_shaper_rate_kbps=1000
+base_ul_shaper_rate_kbps=5000
+max_ul_shaper_rate_kbps=30000
 
-connection_active_thr_kbps=3000
+connection_active_thr_kbps=10
 
+# sisipkan IP Gateway Modem (misal 192.168.8.1) di bagian paling depan daftar ini
 reflectors=(8.8.8.8 1.1.1.1 9.9.9.9 208.67.222.222 1.0.0.1 8.8.4.4)
 
 output_processing_stats=0 
