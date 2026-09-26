76bfa14a1f0fae05514fcff35b92b34b2e9ca659
Atbel <kanaan.waropen@gmail.com>
2026-09-26 16:48:47 +0900

    Update config.primary.sh

--- files/root/cake-autorate/config.primary.sh
+++ files/root/cake-autorate/config.primary.sh
@@ -3,22 +3,23 @@
 # for FRDMX-Wrt Community
 
 ul_if="TEMPLATE_WAN"
-dl_if="lo" 
+dl_if="lo"
 
 adjust_dl_shaper_rate=1 
 adjust_ul_shaper_rate=1 
 
 min_dl_shaper_rate_kbps=3000
-base_dl_shaper_rate_kbps=15000
-max_dl_shaper_rate_kbps=80000
-
 min_ul_shaper_rate_kbps=1000
+
+base_dl_shaper_rate_kbps=15000
 base_ul_shaper_rate_kbps=5000
-max_ul_shaper_rate_kbps=30000
+
+max_dl_shaper_rate_kbps=1000000
+max_ul_shaper_rate_kbps=300000
 
 connection_active_thr_kbps=10
 
-# sisipkan IP Gateway Modem (misal 192.168.8.1) di bagian paling depan daftar ini
+# Catatan: Pengguna bisa menyelipkan IP Gateway Modem mereka di urutan paling depan
 reflectors=(8.8.8.8 1.1.1.1 9.9.9.9 208.67.222.222 1.0.0.1 8.8.4.4)
 
 output_processing_stats=0 
